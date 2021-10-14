bf018074810577fbe90e2def7bb934be482b9887
nandinichhajed <nandinichhajed08@gmail.com>
2021-10-14 23:26:52 +0530

    updating urls

--- liberarymanagement/liberarymanagement/urls.py
+++ liberarymanagement/liberarymanagement/urls.py
@@ -1,7 +1,7 @@
-"""liberarymanagement URL Configuration
+"""librarymanagement URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from liberary import views
+from django.contrib.auth.views import LoginView,LogoutView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('django.contrib.auth.urls') ),
+    path('', views.home_view),
+
+    path('adminclick', views.adminclick_view),
+    path('studentclick', views.studentclick_view),
+
+
+    path('adminsignup', views.adminsignup_view),
+    path('studentsignup', views.studentsignup_view),
+    path('adminlogin', LoginView.as_view(template_name='library/adminlogin.html')),
+    path('studentlogin', LoginView.as_view(template_name='library/studentlogin.html')),
+
+    path('logout', LogoutView.as_view(template_name='library/index.html')),
+    path('afterlogin', views.afterlogin_view),
+
+    path('addbook', views.addbook_view),
+    path('viewbook', views.viewbook_view),
+    path('issuebook', views.issuebook_view),
+    path('viewissuedbook', views.viewissuedbook_view),
+    path('viewstudent', views.viewstudent_view),
+    path('viewissuedbookbystudent', views.viewissuedbookbystudent),
+
+    path('aboutus', views.aboutus_view),
+    path('contactus', views.contactus_view),
+
 ]
