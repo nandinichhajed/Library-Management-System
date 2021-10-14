0f28cb0574261b9b376a5051f135908ab4cab081
nandinichhajed <nandinichhajed08@gmail.com>
2021-10-14 23:26:37 +0530

    updating models, views and admin files

--- liberarymanagement/liberary/models.py
+++ liberarymanagement/liberary/models.py
@@ -1,52 +1,50 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime, timedelta
-
-# Create your models here.
+from datetime import datetime,timedelta
 
 class StudentExtra(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    enrollment = models.CharField(max_length=50)
-    branch = models.CharField(max_length=50)
-    
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    enrollment = models.CharField(max_length=40)
+    branch = models.CharField(max_length=40)
+    #used in issue book
     def __str__(self):
-        return self.user.first_name + '['+str(self.enrollment)+']'
-
+        return self.user.first_name+'['+str(self.enrollment)+']'
     @property
     def get_name(self):
         return self.user.first_name
-
     @property
     def getuserid(self):
         return self.user.id
 
+
 class Book(models.Model):
-    catchoices = [
+    catchoice= [
         ('education', 'Education'),
         ('entertainment', 'Entertainment'),
-        ('comics', 'comics'),
-        ('biography', 'biography'),
-        ('history', 'history'),
-        ('novel', 'novel'),
-        ('fantasy', 'fantasy'),
-        ('thriller', 'thriller'),
-        ('romance', 'romance'),
-        ('scifi', 'Sci-Fi'), 
-    ]
-    name = models.CharField(max_length=50)
+        ('comics', 'Comics'),
+        ('biography', 'Biography'),
+        ('history', 'History'),
+        ('novel', 'Novel'),
+        ('fantasy', 'Fantasy'),
+        ('thriller', 'Thriller'),
+        ('romance', 'Romance'),
+        ('scifi','Sci-Fi')
+        ]
+    name = models.CharField(max_length=30)
     isbn = models.PositiveIntegerField()
-    author = models.CharField(max_length=50)
-    category = models.CharField(max_length=50,choices=catchoices,default='education')
+    author = models.CharField(max_length=40)
+    category = models.CharField(max_length=30,choices=catchoice,default='education')
+    def __str__(self):
+        return str(self.name)+"["+str(self.isbn)+']'
 
-    def get_expiry():
-        return datetime.today() + timedelta(days = 10)
+
+def get_expiry():
+    return datetime.today() + timedelta(days=15)
 
 class IssuedBook(models.Model):
-    enrollment = models.CharField(max_length=50)
-    isbn = models.CharField(max_length=50)
+    enrollment = models.CharField(max_length=30)
+    isbn = models.CharField(max_length=30)
     issuedate = models.DateField(auto_now=True)
-    expirydate = models.DateField(default = get_expiry)
-
+    expirydate = models.DateField(default=get_expiry)
     def __str__(self):
         return self.enrollment
-
